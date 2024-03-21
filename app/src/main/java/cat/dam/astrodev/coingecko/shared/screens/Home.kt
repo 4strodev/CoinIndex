68543afe2eda1a7ae30bd05b2294c668f6fb4cf9
janirahuesca-inspladelestany <janirahuesca@inspladelestany.cat>
2024-03-21 19:39:51 +0100

    Nav bar, home screen and info screen done

--- app/src/main/java/cat/dam/astrodev/coingecko/shared/screens/Home.kt
+++ app/src/main/java/cat/dam/astrodev/coingecko/shared/screens/Home.kt
@@ -1,45 +1,121 @@
 package cat.dam.astrodev.coingecko.shared.screens
 
+import android.app.Activity
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.EaseInElastic
+import androidx.compose.animation.core.EaseInOutCirc
+import androidx.compose.animation.core.EaseOutBounce
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import cat.dam.astrodev.coingecko.AppSurface
 import cat.dam.astrodev.coingecko.R
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import cat.dam.astrodev.coingecko.shared.composables.AnimationItem
+import cat.dam.astrodev.coingecko.ui.theme.appTitleFont
 
 @Composable
 fun Home(navController: NavController) {
-    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
+    var isAnimationFinished by remember { mutableStateOf(false) }
+
+    LaunchedEffect(true) {
+        delay(500)
+        isAnimationFinished = true
+    }
+
+    val scaleValue by animateFloatAsState(
+        targetValue = if (isAnimationFinished) 1f else 0.5f,
+        animationSpec = tween(
+            durationMillis = 1000,
+            easing = EaseInElastic
+        ),
+        label = "Scale animation"
+    )
+
+    val rotationValue by animateFloatAsState(
+        targetValue = if (isAnimationFinished) 360f else 0f,
+        animationSpec = tween(
+            durationMillis = 2000
+        ),
+        label = "Rotation animation"
+    )
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        AnimationItem(
-            animationResId = R.raw.bitcoin_wallet_animation,
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(1f),
-            callback = {
-                scope.launch {
-                    delay(3000)
-                    navController.navigate("coinList")
-                }
-            }
-        )
+        item {
+            // App Name
+            Text(
+                text = stringResource(id = R.string.txt_app_title),
+                modifier = Modifier
+                    .scale(scaleValue),
+                style = MaterialTheme.typography.titleLarge,
+                fontFamily = appTitleFont,
+                color = MaterialTheme.colorScheme.secondary,
+                fontSize = 64.sp
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.bitcoin),
+                contentDescription = null,
+                modifier = Modifier
+                    .graphicsLayer(rotationZ = rotationValue)
+                    .animateContentSize(),
+                contentScale = ContentScale.Fit
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = stringResource(id = R.string.txt_exit),
+                modifier = Modifier
+                    .clickable {
+                        val activity = context as? Activity
+                        activity?.finishAffinity()
+                    }
+                    .background(MaterialTheme.colorScheme.background),
+                color = MaterialTheme.colorScheme.onBackground,
+                textDecoration = TextDecoration.Underline
+            )
+        }
     }
 }
 
--- app/src/main/java/cat/dam/astrodev/coingecko/ui/theme/Type.kt
+++ app/src/main/java/cat/dam/astrodev/coingecko/ui/theme/Type.kt
@@ -2,9 +2,19 @@ package cat.dam.astrodev.coingecko.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import cat.dam.astrodev.coingecko.R
+
+val appTitleFont = FontFamily(
+    Font(R.font.nixie_one, FontWeight.Normal)
+)
+
+val creditsFont = FontFamily(
+    Font(R.font.geosans_light, FontWeight.Light)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
